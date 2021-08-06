37bf5b04636a049e1ac5a76ba6b535f9e1f6b8ad
lamproslntz <lambroslntz15@gmail.com>
2021-08-06 20:22:32 +0300

    added synonym analyzer based on wordnet

---
+++ src/main/java/analyzers/WordnetSynonymAnalyzer.java
@@ -0,0 +1,125 @@
+package analyzers;
+
+import org.apache.lucene.analysis.*;
+import org.apache.lucene.analysis.en.EnglishPossessiveFilter;
+import org.apache.lucene.analysis.en.PorterStemFilter;
+import org.apache.lucene.analysis.miscellaneous.SetKeywordMarkerFilter;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.analysis.standard.StandardTokenizer;
+import org.apache.lucene.analysis.synonym.SynonymGraphFilter;
+import org.apache.lucene.analysis.synonym.SynonymMap;
+import org.apache.lucene.analysis.synonym.WordnetSynonymParser;
+
+import java.io.*;
+import java.text.ParseException;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author Lampros Lountzis
+ */
+public final class WordnetSynonymAnalyzer extends StopwordAnalyzerBase {
+
+    /**
+     * An unmodifiable set containing some common English words that are not usually useful
+     * for searching.
+     */
+    public static final CharArraySet ENGLISH_STOP_WORDS_SET;
+
+    static {
+        final List<String> stopWords = Arrays.asList(
+                "a", "an", "and", "are", "as", "at", "be", "but", "by",
+                "for", "if", "in", "into", "is", "it",
+                "no", "not", "of", "on", "or", "such",
+                "that", "the", "their", "then", "there", "these",
+                "they", "this", "to", "was", "will", "with"
+        );
+        final CharArraySet stopSet = new CharArraySet(stopWords, false);
+        ENGLISH_STOP_WORDS_SET = CharArraySet.unmodifiableSet(stopSet);
+    }
+
+    private final CharArraySet stemExclusionSet;
+
+    /**
+     * Builds an analyzer with the default stop words: {@link #getDefaultStopSet}.
+     */
+    public WordnetSynonymAnalyzer() {
+        this(ENGLISH_STOP_WORDS_SET);
+    }
+
+    /**
+     * Builds an analyzer with the given stop words.
+     *
+     * @param stopwords a stopword set
+     */
+    public WordnetSynonymAnalyzer(CharArraySet stopwords) {
+        this(stopwords, CharArraySet.EMPTY_SET);
+    }
+
+    /**
+     * Builds an analyzer with the given stop words. If a non-empty stem exclusion set is
+     * provided this analyzer will add a {@link SetKeywordMarkerFilter} before
+     * stemming.
+     *
+     * @param stopwords a stopword set
+     * @param stemExclusionSet a set of terms not to be stemmed
+     */
+    public WordnetSynonymAnalyzer(CharArraySet stopwords, CharArraySet stemExclusionSet) {
+        super(stopwords);
+        this.stemExclusionSet = CharArraySet.unmodifiableSet(CharArraySet.copy(stemExclusionSet));
+    }
+
+    /**
+     * Returns an unmodifiable instance of the default stop words set.
+     *
+     * @return default stop words set.
+     */
+    public static CharArraySet getDefaultStopSet(){
+        return ENGLISH_STOP_WORDS_SET;
+    }
+
+    /**
+     * Creates a
+     * {@link org.apache.lucene.analysis.Analyzer.TokenStreamComponents}
+     * which tokenizes all the text in the provided {@link Reader}.
+     *
+     * @return A
+     *         {@link org.apache.lucene.analysis.Analyzer.TokenStreamComponents}
+     *         built from an {@link StandardTokenizer} filtered with
+     *         {@link EnglishPossessiveFilter},
+     *         {@link LowerCaseFilter}, {@link StopFilter}
+     *         , {@link SetKeywordMarkerFilter} if a stem exclusion set is
+     *         provided and {@link PorterStemFilter}.
+     */
+    @Override
+    protected TokenStreamComponents createComponents(String fieldName) {
+        SynonymMap synonymMap;
+        try {
+            Reader reader = new InputStreamReader(new FileInputStream(".\\src\\main\\resources\\prolog\\wn_s.pl"));
+
+            SynonymMap.Builder parser = new WordnetSynonymParser(true, true, new StandardAnalyzer(CharArraySet.EMPTY_SET));
+            ((WordnetSynonymParser) parser).parse(reader);
+
+            synonymMap = parser.build();
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        final Tokenizer source = new StandardTokenizer();
+        TokenStream result = new EnglishPossessiveFilter(source);
+        result = new LowerCaseFilter(result);
+        result = new StopFilter(result, stopwords);
+        result = new SynonymGraphFilter(result, synonymMap, true);
+        if(!stemExclusionSet.isEmpty()) {
+            result = new SetKeywordMarkerFilter(result, stemExclusionSet);
+        }
+        result = new PorterStemFilter(result);
+        return new TokenStreamComponents(source, result);
+    }
+
+    @Override
+    protected TokenStream normalize(String fieldName, TokenStream in) {
+        return new LowerCaseFilter(in);
+    }
+}
