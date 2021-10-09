08414ecd6a1b10e476c59b86d57b4d3f16ff0ad1
lamproslntz <lambroslntz15@gmail.com>
2021-10-09 21:23:07 +0300

    finished search method

--- src/main/java/com/lamproslntz/searchengineextended/controller/SearchController.java
+++ src/main/java/com/lamproslntz/searchengineextended/controller/SearchController.java
@@ -1,9 +1,11 @@
 package com.lamproslntz.searchengineextended.controller;
 
-import com.lamproslntz.searchengineextended.dto.RetrievedItem;
-import com.lamproslntz.searchengineextended.dto.UserQuery;
+import com.lamproslntz.searchengineextended.dto.DocumentDTO;
+import com.lamproslntz.searchengineextended.dto.QueryDTO;
 import com.lamproslntz.searchengineextended.index.Searcher;
 import org.apache.lucene.queryparser.classic.ParseException;
+import org.deeplearning4j.models.embeddings.loader.WordVectorSerializer;
+import org.deeplearning4j.models.word2vec.Word2Vec;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -16,15 +18,15 @@ import java.util.List;
 @RestController
 public class SearchController {
 
-//    private final String MODEL_PATH = "src/main/resources/fasttext-en/wiki-news-300d-1M.vec";
-//    private final Word2Vec MODEL = WordVectorSerializer.readWord2VecModel(MODEL_PATH);
-//
-//    private final double MIN_ACCURACY = 0.98;
+    private final Searcher searcher;
+
+    public SearchController() {
+        Word2Vec model = WordVectorSerializer.readWord2VecModel("src/main/resources/fasttext-en/wiki-news-300d-1M.vec");
+        this.searcher = new Searcher("src/main/resources/index", model, 0.98);
+    }
 
     @PostMapping("/search")
-    public ModelAndView search(@ModelAttribute("userQuery") UserQuery userQuery) {
-        Searcher searcher = new Searcher("src/main/resources/index");
-
+    public ModelAndView search(@ModelAttribute("userQuery") QueryDTO queryDTO) {
         try {
             searcher.open();
         } catch (IOException e) {
@@ -32,9 +34,9 @@ public class SearchController {
             e.printStackTrace();
         }
 
-       List<RetrievedItem> results = null;
+       List<DocumentDTO> results = null;
         try {
-            results = searcher.search(userQuery, 20);
+            results = searcher.search(queryDTO, 20);
         } catch (IOException e) {
             // TODO: logging
             e.printStackTrace();
