60bd90ac5e1b46ada1264f586ae51f13b5d3403c
lamproslntz <lambroslntz15@gmail.com>
2021-10-11 12:14:40 +0300

    documentation pt.3

--- src/main/java/com/lamproslntz/searchengineextended/cleaner/TextCleaner.java
+++ src/main/java/com/lamproslntz/searchengineextended/cleaner/TextCleaner.java
@@ -6,6 +6,9 @@ import java.util.Map;
 import java.util.regex.PatternSyntaxException;
 
 /**
+ * Represents a TextCleaner module, for cleaning a set of documents. Each document consists of a set of fields, which
+ * will be cleaned by lowercasing the text, removing symbols from it and redundant spaces.
+ *
  * @author Lampros Lountzis
  */
 public class TextCleaner implements TextCleanerInterface {
@@ -14,49 +17,79 @@ public class TextCleaner implements TextCleanerInterface {
     private final boolean REMOVE_SYMBOLS;
     private final String SYMBOLS;
 
+    /**
+     * Initializes TextCleaner.
+     *
+     * @param toLowercase boolean flag for whether to lowercase text.
+     * @param removeSymbols boolean flag for whether to remove symbols.
+     */
     public TextCleaner(boolean toLowercase, boolean removeSymbols) {
         this.TO_LOWERCASE = toLowercase;
         this.REMOVE_SYMBOLS = removeSymbols;
         this.SYMBOLS = "[^a-zA-Z0-9]";
     }
 
-    public TextCleaner(boolean toLowercase, boolean removeSymbols, String symbols) {
+    /**
+     * Initializes TextCleaner.
+     *
+     * @param toLowercase boolean flag for whether to lowercase text.
+     * @param symbols string of symbols to be removed, as a regex.
+     */
+    public TextCleaner(boolean toLowercase, String symbols) {
         this.TO_LOWERCASE = toLowercase;
-        this.REMOVE_SYMBOLS = removeSymbols;
+        this.REMOVE_SYMBOLS = true;
         this.SYMBOLS = symbols;
     }
 
-    public void clean(List<Map<String, String>> objects, String[] fields) {
-        for (Map<String, String> obj : objects) {
+    /**
+     * Cleans document fields.
+     * The following operations take place:
+     *  * lowercasing text,
+     *  * removal of symbols,
+     *  * removal of redundant spaces.
+     *
+     * @param text documents and their fields, as a list of dictionaries.
+     * @param fields document fields to be cleaned.
+     */
+    public void clean(List<Map<String, String>> text, String[] fields) {
+        for (Map<String, String> txt : text) {
             for (String field : fields) { // for each obj field to be cleaned
-                if (obj.containsKey(field)) { // if the field exists, clean it
+                if (txt.containsKey(field)) { // if the field exists, clean it
                     if (TO_LOWERCASE) {
-                        obj.put(field, obj.get(field).toLowerCase(Locale.ROOT));
+                        txt.put(field, txt.get(field).toLowerCase(Locale.ROOT));
                     }
 
                     if (REMOVE_SYMBOLS) {
                         try {
-                            obj.put(field, obj.get(field).replaceAll(SYMBOLS, " "));
+                            txt.put(field, txt.get(field).replaceAll(SYMBOLS, " "));
                         } catch (PatternSyntaxException e) {
                             System.out.println("[ERROR] cleaner.TextCleaner.clean - Invalid regular expression pattern.");
                         }
                     }
 
                     // remove redundant spaces
-                    obj.put(field, obj.get(field).replaceAll("\\s+", " "));
-                    obj.put(field, obj.get(field).trim());
+                    txt.put(field, txt.get(field).replaceAll("\\s+", " "));
+                    txt.put(field, txt.get(field).trim());
                 }
             }
         }
     }
 
-    public void fix(List<Map<String, String>> objects, String[] fields) {
-        for (Map<String, String> obj : objects) {
+    /**
+     * Fixes document field structure.
+     * The following operations take place:
+     *  * remove redundant spaces.
+     *
+     * @param text documents and their fields, as a list of dictionaries.
+     * @param fields document fields to be fixed.
+     */
+    public void fix(List<Map<String, String>> text, String[] fields) {
+        for (Map<String, String> txt : text) {
             for (String field : fields) { // for each obj field to be fixed
-                if (obj.containsKey(field)) { // if the field exists, fix it
+                if (txt.containsKey(field)) { // if the field exists, fix it
                     // remove redundant spaces
-                    obj.put(field, obj.get(field).replaceAll("\\s+", " "));
-                    obj.put(field, obj.get(field).trim());
+                    txt.put(field, txt.get(field).replaceAll("\\s+", " "));
+                    txt.put(field, txt.get(field).trim());
                 }
             }
         }
--- src/main/java/com/lamproslntz/searchengineextended/cleaner/TextCleanerInterface.java
+++ src/main/java/com/lamproslntz/searchengineextended/cleaner/TextCleanerInterface.java
@@ -4,10 +4,20 @@ import java.util.List;
 import java.util.Map;
 
 /**
+ * A TextCleaner cleans a set of documents. Each document consists of a set of fields (e.g. ID, title, author, etc.)
+ * each of which can be changed (cleaned) by the TextCleaner. In order to define the cleaning behaviour, subclasses
+ * must define the clean({@link List}<{@link Map}<{@link String}, {@link String}>>, {@link String}) method.
+ *
  * @author Lampros Lountzis
  */
 public interface TextCleanerInterface {
 
+    /**
+     * Specifies the operation of cleaning documents.
+     *
+     * @param text documents and their fields, as a list of dictionaries.
+     * @param fields document fields to be cleaned.
+     */
     void clean(List<Map<String, String>> text, String[] fields);
 
 }
